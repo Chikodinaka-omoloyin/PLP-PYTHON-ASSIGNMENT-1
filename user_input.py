94eabcaacc6279e5829f4c24384231c932743777
Chikodinaka Segun Omoloyin <chikodinaka9@gmail.com>
2024-11-08 21:50:20 +0100

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,4 +1,8 @@
+# Prompts the user to enter their name and store it in the variable 'name'
 name=input("What is your name")
+# Prompts the user to enter their age and store it in the variable 'age'
 age=int(input("How old are you"))
+# Prompts the user to enter their location and store it in the variable 'location'
 location=input("Where do you live?")
+# Print a personalized message that includes the user's name, age, and location.
 print(f"Hello {name}, you are {age} years old and you live in {location}")
